c15a954e034e6f711581154f80f2942ec4a6826a
VNE <vn@abalin.net>
2021-09-06 17:52:50 +0200

    mailer

--- config/mail.php
+++ config/mail.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('MAIL_MAILER', 'ses'),
+    'default' => env('MAIL_MAILER', 'smtp'),
 
     /*
     |--------------------------------------------------------------------------
@@ -92,7 +92,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'admin@abalin.net'),
+        'address' => env('MAIL_FROM_ADDRESS', 'admin@daniela-fitness.cz'),
         'name' => env('MAIL_FROM_NAME', 'VNE'),
     ],
 

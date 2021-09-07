640bcb43119ea1e8b500218f554acfbb2125391d
vne <vn@abalin.net>
2021-09-07 11:18:31 +0200

    detail firmy

--- resources/views/address.blade.php
+++ resources/views/address.blade.php
@@ -3,59 +3,13 @@
 @section('content')
 
     <table class="table" id="list">
-
-        <tr>
-            <th>
-                Address
-            </th>
-            <td> {{$address->address}}</td>
-        </tr>
-        <tr>
-            <th>
-                District
-            </th>
-            <td> {{$address->district}}</td>
-        </tr>
-
-        <tr>
-            <th>
-                Address 2
-            </th>
-            <td> {{$address->address2}}</td>
-        </tr>
-
-        <tr>
-            <th>
-              City ID
-            </th>
-            <td> {{$address->city_id}}</td>
-        </tr>
-
-        <tr>
-            <th>
-                postal code
-            </th>
-            <td> {{$address->postal_code}}</td>
-        </tr>
-
-
-        <tr>
-            <th>
-                Phone
-            </th>
-            <td> {{$address->phone}}</td>
-        </tr>
-
-
-        <tr>
-            <th>
-                Location
-            </th>
-            <td> {{$address->location}}</td>
-        </tr>
+    @foreach(array_keys($address->toArray()) as $item)
+            <tr>
+                <th>{{\Illuminate\Support\Str::ucfirst($item)}}</th>
+                <td>{{$address->$item}}</td>
+            </tr>
+    @endforeach
     </table>
-
-
     <form method="post" action="{{route('sendaddress')}}">
         @csrf
         <div class="form-group">
